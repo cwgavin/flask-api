9595263b726e32bb6b27d2c7a6a55eaf994f6086
Gavin Cheng <cwmima@hotmail.com>
2023-08-02 17:53:15 +1200

    test test

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
-import io
-from imageClassification import predict
+# import io
+# from imageClassification import predict
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Flask API get endpoint running"
+    return "Flask API test test"
 
 
-@app.route("/test", methods=["GET", "POST"])
-def image_classification():
-    file = request.files["file"]
-    fileBytes = io.BytesIO(file.read())
-    return predict(fileBytes, app.logger)
+# @app.route("/test", methods=["GET", "POST"])
+# def image_classification():
+#     file = request.files["file"]
+#     fileBytes = io.BytesIO(file.read())
+#     return predict(fileBytes, app.logger)
 
 
 if __name__ == "__main__":
