ce6910572256cf659a9a2d5603910f66c535a038
Gavin Cheng <cwmima@hotmail.com>
2023-08-02 17:05:33 +1200

    build image classification api

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-import flask
+from flask import Flask, request
+import io
+from imageClassification import predict
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return "Flask API get endpoint running"
+
+
+@app.route("/test", methods=["GET", "POST"])
+def image_classification():
+    file = request.files["file"]
+    fileBytes = io.BytesIO(file.read())
+    return predict(fileBytes, app.logger)
 
 
 if __name__ == "__main__":
